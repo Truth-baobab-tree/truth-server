3fc563ef2286f5c20357591f0831b139b1df12c8
KHJcode <kimstudswcode4485@gmail.com>
2020-11-07 02:05:40 +0900

    parsing update

--- routes/info.js
+++ routes/info.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const request = require('request');
 
-
 router.get('/get/news/sample/:query/:display', (req, res, next) => {
   try {
     const { query, display } = req.params;
@@ -48,5 +47,32 @@ router.get('/get/news/sample/:query/:display', (req, res, next) => {
     next(err);
   }
 });
+
+router.get('/get/factcheck/:query', async (req, res, next)  => {
+  try {
+
+    const { query } = req.params;
+
+    const url = 'https://factcheck.snu.ac.kr';
+
+    request({ uri: `${url}/v2/search?keyword=${encodeURI(query)}`,
+    }, (err, response, body) => {
+      let data = body.replace(/&quot;/g, '').substring(body.indexOf('<div class="fcItem_wrap">'), body.indexOf('<a class="last "'));
+      data = data.substring(data.indexOf('<li class="fcItem_wrap_li">'), data.indexOf('<a class="btn_detail')+20);
+      console.log(data);
+      data = data.substring(data.indexOf('<a href="/v2/facts'), data.indexOf('</a>'));
+      console.log(data)
+      const content = {
+        title: data.substring(data.indexOf('">')+2, 100),
+        link: `${url}/${data.substring(data.indexOf('href="')+7, data.indexOf('">'))}`,
+      };
+      res.status(200).json(content);
+    });
+
+  } catch (err) {
+    next(err);
+  }
+});
+
 
 module.exports = router;
