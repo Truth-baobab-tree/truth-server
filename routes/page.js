231edb313c910d034afc17960f63248eae616bb8
KHJcode <kimstudswcode4485@gmail.com>
2020-11-14 09:28:23 +0900

    add update api && get status api

--- routes/page.js
+++ routes/page.js
@@ -2,9 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 const { User, Page, sequelize } = require('../models');
-const { getUserCheck } = require('../script/user');
+const { getUserCheck, getUserData } = require('../script/user');
 const { getPageCheck } = require('../script/page');
 
+
+router.post('/get/status', async (req, res) => {
+  try {
+    const { url, key } = req.body;
+    const user = await getUserData(key);
+    if (!user || !url) return res.status(300).json('data is losted');
+
+    const data = await Page.findOne({ attributes: ['status', 'reason'], where: { person: user.id, url }});
+
+    res.status(200).json(data);
+
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.post('/get/score', async (req, res, next) => {
   try {
@@ -90,18 +105,41 @@ router.post('/new/eval', async (req, res, next) => {
     const createdAt = `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}`;
 
     if (page) {
-      res.status(200).json('defined');
-    } else {
-      await User.update({ count: user.count + 1 }, { where: { id }});
-      await Page
-        .create({ url, status, person: id, reason, createdAt, })
-        .then(result => {
-          res.status(201).json(result ? 'success' : 'fail');
-        });
+      return res.status(200).json('defined');
     }
+    
+    await User.update({ count: user.count + 1 }, { where: { id }});
+
+    await Page
+      .create({ url, status, person: id, reason, createdAt, })
+      .then(result => res.status(201).json(result ? 'success' : 'fail'));
+      
   } catch (err) {
     next(err);
   }
+});
+
+router.post('/update/eval', async (req, res) => {
+  const { key, url, status, reason } = req.body;
+
+  const user = await getUserData(key);
+
+  if (!user || !url || !reason) {
+    return res.status(300).json('data is losted');
+  }
+  
+  const person = user.id;
+
+  const page = await getPageCheck(url, person);
+
+  if (!page) {
+    return res.status(200).json('undefined');
+  }
+
+  await Page
+    .update({ status, reason }, { where: { url, person }})
+    .then(result => res.status(201).json(result ? 'success' : 'fail'));
+
 });
 
 module.exports = router;
